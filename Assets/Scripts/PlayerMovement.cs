0933525e3d2e959de88b41513eda2636dcd72a4f
Ahsan <ahsanaz461@gmail.com>
2023-11-25 21:39:06 +0700

    feat: Add player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Vector2 movement;
+
+    [Header("Attributes")]
+    public float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rb == null)
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal"), moveY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveX, moveY);
+        rb.MovePosition(rb.position + moveSpeed * movement * Time.fixedDeltaTime);
+    }
+}
